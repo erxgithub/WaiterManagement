3696f9e16f879beebc0f4fc5f01b3164ddae55aa
erxgithub <erxlink@gmail.com>
2018-03-30 14:02:19 -0400

    Added navigation controller, waiter view controller, and constraints for table view.

---
+++ StaffManagement/WaiterViewController.h
@@ -0,0 +1,13 @@
+//
+//  WaiterViewController.h
+//  StaffManagement
+//
+//  Created by Eric Gregor on 2018-03-30.
+//  Copyright © 2018 Derek Harasen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaiterViewController : UIViewController
+
+@end
---
+++ StaffManagement/WaiterViewController.m
@@ -0,0 +1,43 @@
+//
+//  WaiterViewController.m
+//  StaffManagement
+//
+//  Created by Eric Gregor on 2018-03-30.
+//  Copyright © 2018 Derek Harasen. All rights reserved.
+//
+
+#import "WaiterViewController.h"
+
+@interface WaiterViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+
+@end
+
+@implementation WaiterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)saveButtonPressed:(id)sender {
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
